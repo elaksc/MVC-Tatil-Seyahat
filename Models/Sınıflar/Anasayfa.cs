49fa7184db422c5bb8bce3daf9694d54dd1f7151
elaksc <elakascioglu3425@gmail.com>
2023-01-30 22:02:55 +0300

    ilk commit

---
+++ Models/Sınıflar/Anasayfa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TraversalTrimProject.Models.Sınıflar
+{
+    public class Anasayfa
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Baslik { get; set; }
+        public String Aciklama { get; set; }
+    }
+}
