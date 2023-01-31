96b7a11d6fe0b37381d70896449dcb316528f7f1
elaksc <elakascioglu3425@gmail.com>
2023-01-31 18:42:26 +0300

    Son Aşama

--- Controllers/GirisController.cs
+++ Controllers/GirisController.cs
@@ -22,15 +22,10 @@ namespace TraversalTrimProject.Controllers
             return View();
         }
         [HttpPost]
-<<<<<<< HEAD
+
         public ActionResult Login(Admin ad)
         {
             var bilgiler = c.Admins.FirstOrDefault(x => x.KullaniciAdi == ad.KullaniciAdi && x.Sifre == ad.Sifre);
-=======
-        public ActionResult Login(Admin a)
-        {
-            var bilgiler = c.Admins.FirstOrDefault(x => x.KullaniciAdi == a.KullaniciAdi && x.Sifre == a.Sifre);
->>>>>>> 6c52ceca550db4b1cf8a62c34e2e81d752f2f323
             if (bilgiler != null)
             {
                 FormsAuthentication.SetAuthCookie(bilgiler.KullaniciAdi, false);
@@ -42,6 +37,10 @@ namespace TraversalTrimProject.Controllers
                 return View();
             }
         }
-
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login","Giris");
+        }
     }
 }
